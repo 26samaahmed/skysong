9f53927f851b59b356aacfb80ed6c6e521269713
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-01-01 20:50:04 -0800

    wip

--- main.py
+++ main.py
@@ -9,25 +9,27 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 BASE_URL = os.getenv("BASE_URL")
 
-city = "Irvine"
-URL = BASE_URL + "q=" + city + "&units=metric" + "&appid=" + API_KEY
-print(URL)
+def city_temp():
+  print("Choose a city to find the current temperature")
+  CITY = input()
+  URL = BASE_URL + "q=" + CITY + "&units=metric" + "&appid=" + API_KEY
+  print(URL)
 
-response = requests.get(URL)
-print(response.status_code) # Status code is 200. Add an if so that the code checks that is the status code before retrieving the data
+  response = requests.get(URL)
+  print(response.status_code) # Status code is 200. Add an if so that the code checks that is the status code before retrieving the data
 
-# Retrieving the data in json format
-data = response.json()
-# print(data)
-'''
-   {'coord': {'lon': -117.8231, 'lat': 33.6695}, 'weather': [{'id': 801, 'main': 'Clouds', 'description': 'few clouds', 'icon': '02d'}], 
-   'base': 'stations', 'main': {'temp': 291.72, 'feels_like': 291.02, 'temp_min': 289.72, 'temp_max': 294.16, 'pressure': 1017, 'humidity': 53}, 
-   'visibility': 10000, 'wind': {'speed': 3.6, 'deg': 230}, 'clouds': {'all': 20}, 'dt': 1704153284, 'sys': {'type': 1, 'id': 5876, 'country': 'US', 'sunrise': 1704120948, 'sunset': 1704156797}, 
-   'timezone': -28800, 'id': 5359777, 'name': 'Irvine', 'cod': 200}
-'''
+  # Retrieving the data in json format
+  data = response.json()
+  # print(data)
+  '''
+    {'coord': {'lon': -117.8231, 'lat': 33.6695}, 'weather': [{'id': 801, 'main': 'Clouds', 'description': 'few clouds', 'icon': '02d'}], 
+    'base': 'stations', 'main': {'temp': 291.72, 'feels_like': 291.02, 'temp_min': 289.72, 'temp_max': 294.16, 'pressure': 1017, 'humidity': 53}, 
+    'visibility': 10000, 'wind': {'speed': 3.6, 'deg': 230}, 'clouds': {'all': 20}, 'dt': 1704153284, 'sys': {'type': 1, 'id': 5876, 'country': 'US', 'sunrise': 1704120948, 'sunset': 1704156797}, 
+    'timezone': -28800, 'id': 5359777, 'name': 'Irvine', 'cod': 200}
+  '''
 
-# https://openweathermap.org/weather-data 
+  # https://openweathermap.org/weather-data 
 
-main = data['main']
-temp = main['temp']
-print("Temperature: ",temp) # Prints-> Temperature:  18.27 (celsius)
+  main = data['main']
+  temp = main['temp']
+  print("Temperature:",temp) # Prints-> Temperature:  18.27 (celsius)
