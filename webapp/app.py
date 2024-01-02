e74b62112358afd108db1ad2546be334ec56e716
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-01-01 20:50:04 -0800

    wip

--- webapp/app.py
+++ webapp/app.py
@@ -1,7 +1,10 @@
 # Flask
-from flask import Flask
+from flask import Flask, render_template
+from main import city_temp
+
 app = Flask(__name__)
 
 @app.route("/")
 def display():
-  return "Hello World"
+  result = city_temp()
+  return render_template('index.html' , result=result)
