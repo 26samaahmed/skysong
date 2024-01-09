584119dc173738d121bb316d98ab3764412611eb
Sama Ahmed <111910374+26samaahmed@users.noreply.github.com>
2024-01-09 14:30:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#TODO: start by retrieving the current weather for 1 city
-  #future implementation: have a page at the beginning that asks for what city the person wants to see the weather for
 import requests, json
 import os
 from dotenv import load_dotenv
