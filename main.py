06d7cbb4544995afabbc83035de250e976513950
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-01-09 14:20:42 -0800

    Retrieved more data about cities

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import requests, json
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -19,18 +20,32 @@ def City_temp(city: str):
   # Retrieving the data in json format
   data = response.json()
   # print(data)
-  '''
-    {'coord': {'lon': -117.8231, 'lat': 33.6695}, 'weather': [{'id': 801, 'main': 'Clouds', 'description': 'few clouds', 'icon': '02d'}], 
-    'base': 'stations', 'main': {'temp': 291.72, 'feels_like': 291.02, 'temp_min': 289.72, 'temp_max': 294.16, 'pressure': 1017, 'humidity': 53}, 
-    'visibility': 10000, 'wind': {'speed': 3.6, 'deg': 230}, 'clouds': {'all': 20}, 'dt': 1704153284, 'sys': {'type': 1, 'id': 5876, 'country': 'US', 'sunrise': 1704120948, 'sunset': 1704156797}, 
-    'timezone': -28800, 'id': 5359777, 'name': 'Irvine', 'cod': 200}
-  '''
-
   # https://openweathermap.org/weather-data 
 
   main = data['main']
   temp = main['temp']
-  #feels_like = main['feels_like']
-  #print("Temperature feels like:", feels)
-  #print("Current temperature is:", int(temp))
+  min_temp = main['temp_min']
+  max_temp = main['temp_max']
+  print('Min temp is:', int(min_temp)) # Min temp is: 14
+  print('Max temp is:', int(max_temp)) # Max temp is: 19
+  print('Current temp is:', int(temp)) # Current temp is: 17
+
+
+  # Weather data is an array of 1 element that holds this:
+  # 'weather': [{'id': 801, 'main': 'Clouds', 'description': 'few clouds', 'icon': '02d'}]
+  weather_array = data['weather']
+  weather = weather_array[0]
+  main_clouds = weather['main']
+  print('There is:', main_clouds) # There is: Clouds
+
+
+  sys = data['sys']
+  sunset_utc = sys['sunset']
+  sunrise_utc = sys['sunrise']
+  sunset = datetime.fromtimestamp(sunset_utc)
+  sunrise = datetime.fromtimestamp(sunrise_utc)
+  print(f"Sunset is: {sunset:%H:%M}") # Sunset is: 16:59
+  print(f"Sunrise is: {sunrise:%H:%M}") # Sunrise is: 06:56
+  # TODO: convert from military time to standard time
+  # TODO: Create seperate functions to return each of the data like printed above
   return int(temp)
