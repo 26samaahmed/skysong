babac1e8254919b259f82fea374bb9987c18a392
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-01-10 21:52:38 -0800

    Created separate functions for current weather data

--- main.py
+++ main.py
@@ -10,42 +10,42 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 BASE_URL = os.getenv("BASE_URL")
 
-def City_temp(city: str):
+# Helper function that makes the requests to be called in any function where data needs to be retrieved
+def base_url(city: str):
   URL = BASE_URL + "q=" + city + "&units=metric" + "&appid=" + API_KEY
-  print(URL)
-
   response = requests.get(URL)
-  print("status code:", response.status_code) # Status code is 200. Add an if so that the code checks that is the status code before retrieving the data
+  data = response.json() # Retrieve the data in json format
+  return data
 
-  # Retrieving the data in json format
-  data = response.json()
-  # print(data)
-  # https://openweathermap.org/weather-data 
-
+def city_temp(city: str):
+  data = base_url(city)
   main = data['main']
   temp = main['temp']
-  min_temp = main['temp_min']
+  return int(temp)
+
+def max_temp(city: str):
+  data = base_url(city)
+  main = data['main']
   max_temp = main['temp_max']
-  print('Min temp is:', int(min_temp)) # Min temp is: 14
-  print('Max temp is:', int(max_temp)) # Max temp is: 19
-  print('Current temp is:', int(temp)) # Current temp is: 17
+  return int(max_temp)
 
+def min_temp(city: str):
+  data = base_url(city)
+  main = data['main']
+  min_temp = main['temp_min']
+  return int(min_temp)
 
-  # Weather data is an array of 1 element that holds this:
-  # 'weather': [{'id': 801, 'main': 'Clouds', 'description': 'few clouds', 'icon': '02d'}]
-  weather_array = data['weather']
-  weather = weather_array[0]
-  main_clouds = weather['main']
-  print('There is:', main_clouds) # There is: Clouds
-
-
+# TODO: convert from military time to standard time
+def sunset_time(city: str):
+  data = base_url(city)
   sys = data['sys']
   sunset_utc = sys['sunset']
-  sunrise_utc = sys['sunrise']
   sunset = datetime.fromtimestamp(sunset_utc)
+  return f"Sunset is: {sunset:%H:%M}"
+
+def sunrise_time(city: str):
+  data = base_url(city)
+  sys = data['sys']
+  sunrise_utc = sys['sunrise']
   sunrise = datetime.fromtimestamp(sunrise_utc)
-  print(f"Sunset is: {sunset:%H:%M}") # Sunset is: 16:59
-  print(f"Sunrise is: {sunrise:%H:%M}") # Sunrise is: 06:56
-  # TODO: convert from military time to standard time
-  # TODO: Create seperate functions to return each of the data like printed above
-  return int(temp)
+  return f"Sunrise is: {sunrise:%H:%M}"
