a6ac622e36b69b5e0a422a3f6d00a9a9417eef57
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-04-03 00:32:54 -0700

    Get Song URIs

--- albums.py
+++ albums.py
@@ -11,8 +11,9 @@ SPOTIFY_CLIENT_SECRET=os.getenv("SPOTIFY_CLIENT_SECRET")
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET))
 
 
-def get_track_name(playlist_id: str) -> list:
-  track_names = []
+def get_tempo(playlist_id: str) -> dict:
+  tracks_dict = {}
+  tempo = {}
 
   # Get Playlist using its ID
   playlist = spotify.playlist_items(playlist_id)
@@ -23,75 +24,29 @@ def get_track_name(playlist_id: str) -> list:
     playlist = spotify.next(playlist)
     tracks.extend(playlist['items'])
 
-  # Get each track's name
+  # Get each track's name [made it into a dict where song name is the key and the uri is the value]
   for track in tracks:
-    track_names.append(track['track']['name'])
+    tracks_dict[track['track']['name']] = track['track']['uri']
+    #print(track['track']['name'], ":", track['track']['uri'])
 
-  return track_names
+  return tracks_dict
+  
 
 playlist_id1 = 'spotify:playlist:37i9dQZF1E37LFLUC2Op3u'
 playlist_id2 = 'spotify:playlist:37i9dQZF1E36mu0j5Hw95G'
 playlist_id3 = 'spotify:playlist:37i9dQZF1E36mCM83KV1pv'
-r1 = get_track_name(playlist_id1)
-r2 = get_track_name(playlist_id2)
-r3 = get_track_name(playlist_id3)
+r1 = get_tempo(playlist_id1)
+r2 = get_tempo(playlist_id2)
+r3 = get_tempo(playlist_id3)
 print(r1)
 print(r2)
 print(r3)
 
-#TODO: Get the tempo for each song now that I have the names. Reference code below
+#TODO: Get the tempo for each song now that I have the names.
 
 '''
-song_name = []
-song_uri = []
-album = []
-count = 0
-for j in uri:
-    
-    tracks = sp.album_tracks(j)   
-    for i in tracks['items']:
-        album.append(name[count])
-        song_name.append(i['name'])
-        song_uri.append(i['uri'])
-    count+=1
-song_name
-acoustic = []
-dance = []
-energy = []
-instrumental = []
-liveness = []
-loudness = []
-speech = []
-tempo = []
-valence = []
-popularity = []
-
-for i in song_uri:
-    feat = sp.audio_features(i)[0]
-    acoustic.append(feat['acousticness'])
-    dance.append(feat['danceability'])
-    energy.append(feat['energy'])
-    speech.append(feat['speechiness'])
-    instrumental.append(feat['instrumentalness'])
-    loudness.append(feat['loudness'])
-    tempo.append(feat['tempo'])
-    liveness.append(feat['liveness'])
-    valence.append(feat['valence'])
-    popu = sp.track(i)
-    popularity.append(popu['popularity'])
-    ari = pd.DataFrame({'name':song_name,
-                    'album':album,
-                    'dance':dance,
-                    'acoustic':acoustic,
-                    'energy':energy,
-                    'instrumental':instrumental,
-                    'liveness':liveness,
-                    'loudness':loudness,
-                    'speech':speech,
-                    'tempo':tempo,
-                    'valence':valence,
-                    'popularity':popularity
-    
+ari = pd.DataFrame({'name':song_name,
+                    'tempo':tempo
 })
 ari.head()
   '''
