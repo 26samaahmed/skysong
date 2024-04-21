ced87047c9801501ed815a11de9ff83c73013574
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-04-20 21:28:06 -0700

    Get Track ID

--- calculations.py
+++ calculations.py
@@ -26,37 +26,33 @@ def get_tempo(playlist_id: str) -> dict:
 
   for track in tracks:
     # Retrieve the tempo for each song using its URI
-    #print(track['track']['name'], ":", spotify.audio_features(track['track']['uri'])[0]['tempo'])
     track_tempo[track['track']['name']] = spotify.audio_features(track['track']['uri'])[0]['tempo']
   return track_tempo
+      
 
-#TODO: Do the algorthm that will compare the current temperature to a song with a similar tempo
+def get_track_id(playlist_id):
+    track_id = {}
 
-# Break down to smaller parts: get each individual calculcation and find the correlation
-# Fast tempo: 120bpm < x
-# Medium tempo: 120 bpm > x
-# High temperature: 20ºC > x
-# Low temeprature: 20ºC < x
-def calculate_recommendation(playlist_id, city):
+    # Get Playlist using its ID
+    playlist = spotify.playlist_items(playlist_id)
+    tracks = playlist['items']
 
-  # Loop through the list of songs tempo which is the dict's values
-  # Return the first match I find.
-  # Logic Problem with that is sometimes i would be recommending the same song and not moving on to the next as the hour changes
+    # Ensure I get all the tracks
+    while playlist['next']:
+      playlist = spotify.next(playlist)
+      tracks.extend(playlist['items'])
+
+    for track in tracks:
+      track_id[track['track']['name']] = track['track']['id']
+    return track_id
+
+
+def calculate_recommendation(playlist: dict, city: str):
   temperature = city_temp(city)
   
-  for track in playlist_id:
-    if temperature < 20 and playlist_id[track] < 120:
-      return f"{track}, {playlist_id[track]}, {temperature}"
-    elif temperature > 20 and playlist_id[track] > 120:
-      return f"{track}, {playlist_id[track]}, {temperature}"
+  for track in playlist.copy():
+    if temperature < 20 and playlist[track] < 120:
+      return f"{track}, {playlist[track]}, {temperature}"
       
-  
-id1 = 'spotify:playlist:6178cj0ubqnzFU3q3ovsnm'
-r = get_tempo(id1)
-
-# playlist_id1 = 'spotify:playlist:37i9dQZF1E37LFLUC2Op3u'
-# playlist_id2 = 'spotify:playlist:37i9dQZF1E36mu0j5Hw95G'
-# playlist_id3 = 'spotify:playlist:37i9dQZF1E36mCM83KV1pv'
-
-result = calculate_recommendation(r, "Irvine")
-print(result)
+    elif temperature > 20 and playlist[track] > 120:
+      return f"{track}, {playlist[track]}, {temperature}"
